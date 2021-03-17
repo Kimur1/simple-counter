196055a0130529dad4bcfd17eb5d9de562ba472f
Kimur1 <kimur53@gmail.com>
2021-03-17 20:15:02 +0000

    index.html

--- src/js/icons.js
+++ src/js/icons.js
@@ -1,8 +1,7 @@
-/* global FontAwesomeConfig */
-
-import fontawesome from '@fortawesome/fontawesome';
-import faClock from '@fortawesome/fontawesome-free-regular/faClock';
+/* global FontAwesomeConfig 
+import fontawesome from "@fortawesome/fontawesome";
+import faClock from "@fortawesome/fontawesome-free-regular/faClock";
 fontawesome.config = {
-  autoReplaceSvg: 'nest'
+	autoReplaceSvg: "nest"
 };
-fontawesome.library.add(faClock);
+fontawesome.library.add(faClock); */
--- src/js/index.js
+++ src/js/index.js
@@ -4,12 +4,13 @@ import ReactDOM from "react-dom";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
+//import "./icons.js";
 
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
 //import your own components
-import { count} from "./component/count.js";
+import { count } from "./component/count";
 
 //render your react application
 ReactDOM.render(<count />, document.querySelector("#app"));
